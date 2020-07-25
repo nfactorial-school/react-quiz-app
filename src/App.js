38a9916b09bfa04973c3420dea72b33aa8c31e1d
Bakytzhan Baizhikenov <bahakz@gmail.com>
2020-07-25 22:31:38 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [text, setText] = React.useState("abcd");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Some question</div>
+      <input
+        value={text}
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+      ></input>
+      <button
+        onClick={() => {
+          setText("");
+        }}
+      >
+        Reset
+      </button>
     </div>
   );
 }
